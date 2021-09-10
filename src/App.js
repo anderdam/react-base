9406d21b6d63c7cbcbee1f9928e16051448040a3
anderdam <anderdam@gmail.com>
2021-09-10 15:09:46 -0300

    index-updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import Login from './pages/Login';
+import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
+import Routes from './routes';
+import history from './services/history';
 
 function App() {
   return (
-    <>
+    <Router history={history}>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyles />
-    </>
+      <ToastContainer autoClose={3000} className="toast-container" />
+    </Router>
   );
 }
 
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,14 +5,12 @@ import { Container } from '../../styles/GlobalStyles';
 export default function Login() {
   return (
     <Container>
-      <div>
-        <Title>
-          Login
-          <small>Acessar</small>
-        </Title>
-        <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
-        <button type="button">Enviar</button>
-      </div>
+      <Title>
+        Login
+        <small>Acessar</small>
+      </Title>
+      <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
+      <button type="button">Enviar</button>
     </Container>
   );
 }
